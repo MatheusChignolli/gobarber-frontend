d4bc3e8f58940e5e15f1b83bc69e971b7d017537
Matheus Richard Chignolli <matheuschignolli@gmail.com>
2020-09-11 15:29:54 -0300

    feat: add unform to support form's submit

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Logo from '../../assets/images/logo.svg';
 
@@ -9,13 +10,17 @@ import Input from '../../components/Input';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={Logo} alt="GoBarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
@@ -26,7 +31,7 @@ const SignUp: React.FC = () => {
             placeholder="Senha"
           />
           <Button type="submit">Entrar</Button>
-        </form>
+        </Form>
         <a href="/">
           <FiArrowLeft />
           Voltar para logon
